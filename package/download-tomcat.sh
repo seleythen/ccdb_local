4f4433eb8463f5c3a49a3ca17f7c9f121644c908
Costin Grigoras <costin.grigoras@cern.ch>
2017-10-03 16:59:04 +0200

    Less verbose running

--- package/download-tomcat.sh
+++ package/download-tomcat.sh
@@ -8,12 +8,14 @@ VER="8.5.23"
 T="apache-tomcat-$VER"
 
 if [ ! -d "$T" ]; then
-    wget "http://mirror.switch.ch/mirror/apache/dist/tomcat/tomcat-8/v$VER/bin/apache-tomcat-$VER.tar.gz" -O apache-tomcat.tar.gz || exit 1
+    echo "Downloading Tomcat $VER"
+
+    wget -nv "http://mirror.switch.ch/mirror/apache/dist/tomcat/tomcat-8/v$VER/bin/apache-tomcat-$VER.tar.gz" -O apache-tomcat.tar.gz || exit 1
 
     tar -xf apache-tomcat.tar.gz || exit 2
     rm apache-tomcat.tar.gz
 
-    rm -rfv apache-tomcat-$VER/{conf,logs,temp,webapps,work,LICENSE,NOTICE,RELEASE-NOTES,RUNNING.txt} apache-tomcat-$VER/bin/{*.sh,*.bat,*.tar.gz,*.xml}
+    rm -rf apache-tomcat-$VER/{conf,logs,temp,webapps,work,LICENSE,NOTICE,RELEASE-NOTES,RUNNING.txt} apache-tomcat-$VER/bin/{*.sh,*.bat,*.tar.gz,*.xml}
 fi
 
 rm -f apache-tomcat
