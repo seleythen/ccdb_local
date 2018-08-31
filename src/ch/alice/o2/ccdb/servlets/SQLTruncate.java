76e44519f523b047474af088d85600f76f16d08f
Costin Grigoras <costin.grigoras@cern.ch>
2018-08-31 16:02:56 +0200

    Check if any of the involved folders is now empty and remove it

--- src/ch/alice/o2/ccdb/servlets/SQLTruncate.java
+++ src/ch/alice/o2/ccdb/servlets/SQLTruncate.java
@@ -2,6 +2,7 @@ package ch.alice.o2.ccdb.servlets;
 
 import java.io.IOException;
 import java.util.Collection;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,6 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 import alien.monitoring.Monitor;
 import alien.monitoring.MonitorFactory;
 import ch.alice.o2.ccdb.RequestParser;
+import lazyj.DBFunctions;
 
 /**
  * Remove all matching objects in a single operation.
@@ -48,6 +50,19 @@ public class SQLTruncate extends HttpServlet {
 				response.setHeader("Deleted", matchingObjects.size() + " objects");
 
 				response.sendError(HttpServletResponse.SC_NO_CONTENT);
+
+				final List<Integer> pathIDs = SQLObject.getPathIDsWithPatternFallback(parser);
+
+				if (pathIDs != null)
+					try (DBFunctions db = SQLObject.getDB()) {
+						for (final Integer pathID : pathIDs) {
+							db.query("SELECT 1 FROM ccdb WHERE pathid=? LIMIT 1;", false, pathID);
+
+							if (!db.moveNext())
+								if (db.query("DELETE FROM ccdb_paths WHERE pathid=?;", false, pathID))
+									SQLObject.removePathID(pathID);
+						}
+					}
 			}
 			else
 				response.sendError(HttpServletResponse.SC_NOT_MODIFIED);
