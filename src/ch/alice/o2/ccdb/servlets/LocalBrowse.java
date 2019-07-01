a6d356fe3ab13a99362d948e6185be0a72fe11aa
Costin Grigoras <costin.grigoras@cern.ch>
2019-07-01 15:45:12 +0200

    Add timing for local /browse/ calls

--- src/ch/alice/o2/ccdb/servlets/LocalBrowse.java
+++ src/ch/alice/o2/ccdb/servlets/LocalBrowse.java
@@ -18,6 +18,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import alien.monitoring.Monitor;
+import alien.monitoring.MonitorFactory;
+import alien.monitoring.Timing;
 import ch.alice.o2.ccdb.RequestParser;
 import ch.alice.o2.ccdb.servlets.formatters.HTMLFormatter;
 import ch.alice.o2.ccdb.servlets.formatters.JSONFormatter;
@@ -36,118 +39,122 @@ import lazyj.Format;
 public class LocalBrowse extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
+	private static final Monitor monitor = MonitorFactory.getMonitor(LocalBrowse.class.getCanonicalName());
+
 	@Override
 	protected void doGet(final HttpServletRequest request, final HttpServletResponse response) throws ServletException, IOException {
-		// list of objects matching the request
-		// URL parameters are:
-		// task name / detector name [ / time [ / UUID ] | [ / query string]* ]
-		// if time is missing - get the last available time
-		// query string example: "quality=2"
+		try (Timing t = new Timing(monitor, "GET_ms")) {
+			// list of objects matching the request
+			// URL parameters are:
+			// task name / detector name [ / time [ / UUID ] | [ / query string]* ]
+			// if time is missing - get the last available time
+			// query string example: "quality=2"
 
-		final RequestParser parser = new RequestParser(request, true);
+			final RequestParser parser = new RequestParser(request, true);
 
-		final List<LocalObjectWithVersion> matchingObjects = getAllMatchingObjects(parser);
+			final List<LocalObjectWithVersion> matchingObjects = getAllMatchingObjects(parser);
 
-		Collections.sort(matchingObjects, (o1, o2) -> o1.getPath().compareTo(o2.getPath()));
+			Collections.sort(matchingObjects, (o1, o2) -> o1.getPath().compareTo(o2.getPath()));
 
-		String sContentType;
-		SQLFormatter formatter = null;
+			String sContentType;
+			SQLFormatter formatter = null;
 
-		String sAccept = request.getParameter("Accept");
+			String sAccept = request.getParameter("Accept");
 
-		if ((sAccept == null) || (sAccept.length() == 0))
-			sAccept = request.getHeader("Accept");
+			if ((sAccept == null) || (sAccept.length() == 0))
+				sAccept = request.getHeader("Accept");
 
-		if ((sAccept == null || sAccept.length() == 0))
-			sAccept = "text/plain";
+			if ((sAccept == null || sAccept.length() == 0))
+				sAccept = "text/plain";
 
-		sAccept = sAccept.toLowerCase();
+			sAccept = sAccept.toLowerCase();
 
-		if ((sAccept.indexOf("application/json") >= 0) || (sAccept.indexOf("text/json") >= 0)) {
-			sContentType = "application/json";
-			formatter = new JSONFormatter();
-		}
-		else
-			if (sAccept.indexOf("text/html") >= 0) {
-				sContentType = "text/html";
-				formatter = new HTMLFormatter();
+			if ((sAccept.indexOf("application/json") >= 0) || (sAccept.indexOf("text/json") >= 0)) {
+				sContentType = "application/json";
+				formatter = new JSONFormatter();
 			}
 			else
-				if (sAccept.indexOf("text/xml") >= 0) {
-					sContentType = "text/xml";
-					formatter = new XMLFormatter();
+				if (sAccept.indexOf("text/html") >= 0) {
+					sContentType = "text/html";
+					formatter = new HTMLFormatter();
 				}
-				else {
-					sContentType = "text/plain";
-					formatter = new TextFormatter();
-				}
-
-		response.setContentType(sContentType);
-
-		try (PrintWriter pw = response.getWriter()) {
-			formatter.start(pw);
-
-			formatter.header(pw);
-
-			boolean first = true;
-
-			if (matchingObjects != null)
-				for (final LocalObjectWithVersion object : matchingObjects) {
-					if (first)
-						first = false;
-					else
-						formatter.middle(pw);
-
-					formatter.format(pw, object);
-				}
-
-			formatter.footer(pw);
-
-			if (!parser.wildcardMatching) {
-				// It is not clear which subfolders to list in case of a wildcard matching of objects. As the full hierarchy was included in the search there is no point in showing them, so just skip
-				// this section.
-				formatter.subfoldersListingHeader(pw);
-
-				final String prefix = Local.basePath + "/" + parser.path;
-
-				String suffix = "";
-
-				if (parser.startTimeSet)
-					suffix += "/" + parser.startTime;
-
-				if (parser.uuidConstraint != null)
-					suffix += "/" + parser.uuidConstraint;
-
-				for (final Map.Entry<String, String> entry : parser.flagConstraints.entrySet())
-					suffix += "/" + entry.getKey() + "=" + entry.getValue();
-
-				final File fBaseDir = new File(prefix);
-
-				final File[] baseDirListing = fBaseDir.listFiles((f) -> f.isDirectory());
-
-				first = true;
-
-				if (baseDirListing != null)
-					for (final File fSubdir : baseDirListing) {
-						try {
-							Long.parseLong(fSubdir.getName());
-						}
-						catch (@SuppressWarnings("unused") final NumberFormatException nfe) {
-							if (first)
-								first = false;
-							else
-								formatter.middle(pw);
-
-							final String pathPrefix = parser.path.length() > 0 ? parser.path + "/" : "";
-
-							formatter.subfoldersListing(pw, pathPrefix + fSubdir.getName(), pathPrefix + fSubdir.getName() + suffix);
-						}
+				else
+					if (sAccept.indexOf("text/xml") >= 0) {
+						sContentType = "text/xml";
+						formatter = new XMLFormatter();
+					}
+					else {
+						sContentType = "text/plain";
+						formatter = new TextFormatter();
 					}
 
-				formatter.subfoldersListingFooter(pw, 0, 0);
-			}
+			response.setContentType(sContentType);
 
-			formatter.end(pw);
+			try (PrintWriter pw = response.getWriter()) {
+				formatter.start(pw);
+
+				formatter.header(pw);
+
+				boolean first = true;
+
+				if (matchingObjects != null)
+					for (final LocalObjectWithVersion object : matchingObjects) {
+						if (first)
+							first = false;
+						else
+							formatter.middle(pw);
+
+						formatter.format(pw, object);
+					}
+
+				formatter.footer(pw);
+
+				if (!parser.wildcardMatching) {
+					// It is not clear which subfolders to list in case of a wildcard matching of objects. As the full hierarchy was included in the search there is no point in showing them, so just skip
+					// this section.
+					formatter.subfoldersListingHeader(pw);
+
+					final String prefix = Local.basePath + "/" + parser.path;
+
+					String suffix = "";
+
+					if (parser.startTimeSet)
+						suffix += "/" + parser.startTime;
+
+					if (parser.uuidConstraint != null)
+						suffix += "/" + parser.uuidConstraint;
+
+					for (final Map.Entry<String, String> entry : parser.flagConstraints.entrySet())
+						suffix += "/" + entry.getKey() + "=" + entry.getValue();
+
+					final File fBaseDir = new File(prefix);
+
+					final File[] baseDirListing = fBaseDir.listFiles((f) -> f.isDirectory());
+
+					first = true;
+
+					if (baseDirListing != null)
+						for (final File fSubdir : baseDirListing) {
+							try {
+								Long.parseLong(fSubdir.getName());
+							}
+							catch (@SuppressWarnings("unused") final NumberFormatException nfe) {
+								if (first)
+									first = false;
+								else
+									formatter.middle(pw);
+
+								final String pathPrefix = parser.path.length() > 0 ? parser.path + "/" : "";
+
+								formatter.subfoldersListing(pw, pathPrefix + fSubdir.getName(), pathPrefix + fSubdir.getName() + suffix);
+							}
+						}
+
+					formatter.subfoldersListingFooter(pw, 0, 0);
+				}
+
+				formatter.end(pw);
+			}
 		}
 	}
 
@@ -269,7 +276,7 @@ public class LocalBrowse extends HttpServlet {
 					recursiveMatching(parser, ret, fInterval, matchingPattern);
 				}
 			}
-		
+
 		if (parser.latestFlag && mostRecent != null)
 			ret.add(mostRecent);
 	}
