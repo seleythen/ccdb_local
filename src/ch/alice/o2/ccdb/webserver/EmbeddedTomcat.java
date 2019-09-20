0e897f083dc8b6200c5d01f0463165b97ed3b3c2
Costin Grigoras <costin.grigoras@cern.ch>
2019-09-20 13:48:59 +0200

    Add support for optional HTTPS connector

--- src/ch/alice/o2/ccdb/webserver/EmbeddedTomcat.java
+++ src/ch/alice/o2/ccdb/webserver/EmbeddedTomcat.java
@@ -1,19 +1,35 @@
 package ch.alice.o2.ccdb.webserver;
 
+import java.io.File;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.logging.Level;
 import java.util.logging.LogManager;
+import java.util.logging.Logger;
 
 import javax.servlet.ServletException;
 
 import org.apache.catalina.Globals;
 import org.apache.catalina.LifecycleException;
 import org.apache.catalina.LifecycleState;
+import org.apache.catalina.Service;
 import org.apache.catalina.Valve;
 import org.apache.catalina.Wrapper;
+import org.apache.catalina.authenticator.SSLAuthenticator;
+import org.apache.catalina.connector.Connector;
 import org.apache.catalina.core.StandardContext;
 import org.apache.catalina.core.StandardHost;
 import org.apache.catalina.startup.Tomcat;
 import org.apache.catalina.valves.ErrorReportValve;
+import org.apache.tomcat.util.descriptor.web.LoginConfig;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 
+import alien.monitoring.Monitor;
+import alien.monitoring.MonitorFactory;
+import alien.user.JAKeyStore;
+import alien.user.LdapCertificateRealm;
+import alien.user.UserFactory;
 import ch.alice.o2.ccdb.Options;
 
 /**
@@ -23,6 +39,7 @@ import ch.alice.o2.ccdb.Options;
  * @since 2017-10-13
  */
 public class EmbeddedTomcat extends Tomcat {
+	static transient final Monitor monitor = MonitorFactory.getMonitor(EmbeddedTomcat.class.getCanonicalName());
 
 	static {
 		System.setProperty(Globals.CATALINA_HOME_PROP, System.getProperty("java.io.tmpdir"));
@@ -47,19 +64,59 @@ public class EmbeddedTomcat extends Tomcat {
 		if (debugLevel < 2)
 			LogManager.getLogManager().reset();
 
+		if (debugLevel > 2) {
+			Logger.getLogger("org.apache.catalina").setLevel(Level.FINEST);
+			Logger.getLogger("alien").setLevel(Level.FINEST);
+		}
+
 		address = Options.getOption("tomcat.address", defaultAddress);
 
 		setPort(Options.getIntOption("tomcat.port", 8080));
-		getConnector().setProperty("address", address);
-		getConnector().setProperty("maxKeepAliveRequests", String.valueOf(Options.getIntOption("maxKeepAliveRequests", 1000)));
 
-		getConnector().setProperty("connectionTimeout", String.valueOf(Options.getIntOption("connectionTimeout", 10000))); // clients should be quick
+		decorateConnector(getConnector());
 
-		getConnector().setProperty("disableUploadTimeout", "false");
-		getConnector().setProperty("connectionUploadTimeout", String.valueOf(Options.getIntOption("connectionTimeout", 300000))); // 5 minutes max to upload an object		
-		
 		// Add a dummy ROOT context
-		ctx = (StandardContext) addContext("", null);
+		ctx = (StandardContext) addContext(getHost(), "", null);
+
+		if (Options.getIntOption("ccdb.ssl", 0) > 0)
+			initializeSSLEndpoint();
+		else
+			getConnector().setRedirectPort(0);
+	}
+
+	private void decorateConnector(final Connector connector) {
+		connector.setProperty("address", address);
+		connector.setProperty("maxKeepAliveRequests", String.valueOf(Options.getIntOption("maxKeepAliveRequests", 1000)));
+
+		connector.setProperty("connectionTimeout", String.valueOf(Options.getIntOption("connectionTimeout", 10000))); // clients should be quick
+
+		connector.setProperty("disableUploadTimeout", "false");
+		connector.setProperty("connectionUploadTimeout", String.valueOf(Options.getIntOption("connectionTimeout", 300000))); // 5 minutes max to upload an object
+	}
+
+	/**
+	 * @param connector
+	 * @return <code>true</code> if monitoring was attached to this connector
+	 */
+	public static boolean attachMonitoring(final Connector connector) {
+		final Executor executor = connector.getProtocolHandler().getExecutor();
+
+		if (executor instanceof ThreadPoolExecutor) {
+			final ThreadPoolExecutor tpe = (ThreadPoolExecutor) executor;
+
+			monitor.addMonitoring("server_status_" + connector.getPort(), (names, values) -> {
+				names.add("active_threads");
+				values.add(Double.valueOf(tpe.getActiveCount()));
+
+				names.add("max_threads");
+				values.add(Double.valueOf(tpe.getMaximumPoolSize()));
+			});
+
+			return true;
+		}
+
+		System.err.println("Cannot monitor Tomcat executor on port " + connector.getPort() + (executor != null ? " of type " + executor.getClass().getCanonicalName() : ""));
+		return false;
 	}
 
 	/**
@@ -77,14 +134,20 @@ public class EmbeddedTomcat extends Tomcat {
 	@Override
 	public void start() throws LifecycleException {
 		super.start();
-		if (getService().findConnectors()[0].getState() == LifecycleState.FAILED) {
-			System.err.println("Failed to start the embedded Tomcat listening on " + address + ":" + port + ".");
 
-			if (debugLevel < 2)
-				System.err.println("Set -Dtomcat.debug=2 (or export TOMCAT_DEBUG=2) to see the logging messages from the server.");
+		for (final Connector connector : getService().findConnectors())
+			if (connector.getState() == LifecycleState.FAILED) {
+				System.err.println("Failed to start the embedded Tomcat listening on " + address + ":" + connector.getPort() + ".");
 
-			throw new LifecycleException("Cannot bind on " + address + ":" + port);
-		}
+				if (debugLevel < 2)
+					System.err.println("Set -Dtomcat.debug=2 (or export TOMCAT_DEBUG=2) to see the logging messages from the server.");
+
+				throw new LifecycleException("Cannot bind on " + address + ":" + port);
+			}
+
+		// everything is ok, we can start monitoring the pools
+		for (final Connector connector : getService().findConnectors())
+			attachMonitoring(connector);
 
 		final StandardHost host = (StandardHost) getHost();
 
@@ -107,5 +170,102 @@ public class EmbeddedTomcat extends Tomcat {
 	 */
 	public void blockWaiting() {
 		getServer().await();
+	}
+
+	/**
+	 * @return <code>true</code> if the SSL endpoint could be created ok, <code>false</code> otherwise
+	 */
+	public boolean initializeSSLEndpoint() {
+		final Service service = getService();
+		try {
+			final Connector sslConnector = createSslConnector();
+			service.addConnector(sslConnector);
+		}
+		catch (final Exception e) {
+			System.err.println("Could not initialize the SSL connector: " + e.getMessage());
+			return false;
+		}
+
+		final LdapCertificateRealm ldapRealm = new LdapCertificateRealm();
+		getEngine().setRealm(ldapRealm);
+
+		// Protect all write operations
+		final SecurityCollection defaultPath = new SecurityCollection("defaultPath", "Default path protection");
+		defaultPath.addPattern("/*");
+		defaultPath.addMethod("POST");
+		defaultPath.addMethod("PUT");
+		defaultPath.addMethod("DELETE");
+
+		final SecurityCollection truncateCalls = new SecurityCollection("truncateCalls", "Protect TRUNCATE calls");
+		truncateCalls.addPattern("/truncate/*");
+
+		final String restrictToRole = Options.getOption("ldap.role", "ccdb");
+
+		// Require SSL
+		final SecurityConstraint securityConstraint = new SecurityConstraint();
+		securityConstraint.setDisplayName("SSL certificate required");
+		securityConstraint.addCollection(defaultPath);
+		securityConstraint.addCollection(truncateCalls);
+		securityConstraint.addAuthRole(restrictToRole);
+		securityConstraint.setAuthConstraint(true);
+		securityConstraint.setUserConstraint("CONFIDENTIAL");
+
+		// Use AliEn's LDAP to look up users and roles
+		final LoginConfig loginConfig = new LoginConfig();
+		loginConfig.setRealmName(LdapCertificateRealm.class.getCanonicalName());
+		loginConfig.setAuthMethod("CLIENT-CERT");
+
+		ctx.setLoginConfig(loginConfig);
+		ctx.addSecurityRole(restrictToRole);
+		ctx.addConstraint(securityConstraint);
+		ctx.setRealm(ldapRealm);
+		ctx.getPipeline().addValve(new SSLAuthenticator());
+
+		return true;
+	}
+
+	/**
+	 * Create SSL connector for the Tomcat server
+	 *
+	 * @param tomcatPort
+	 * @throws Exception
+	 */
+	private Connector createSslConnector() throws Exception {
+		final int tomcatPort = Options.getIntOption("tomcat.port.ssl", 8443);
+
+		getConnector().setRedirectPort(tomcatPort);
+
+		final String keystorePass = new String(JAKeyStore.pass);
+
+		final String dirName = System.getProperty("java.io.tmpdir") + File.separator;
+		final String keystoreName = dirName + "keystore.jks_" + UserFactory.getUserID();
+		final String truststoreName = dirName + "truststore.jks_" + UserFactory.getUserID();
+
+		if (!JAKeyStore.saveKeyStore(JAKeyStore.getKeyStore(), keystoreName, JAKeyStore.pass))
+			return null;
+
+		if (!JAKeyStore.saveKeyStore(JAKeyStore.trustStore, truststoreName, JAKeyStore.pass))
+			return null;
+
+		final Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+
+		connector.setPort(tomcatPort);
+		connector.setSecure(true);
+		connector.setScheme("https");
+		connector.setAttribute("keyAlias", "User.cert");
+		connector.setAttribute("keystorePass", keystorePass);
+		connector.setAttribute("keystoreType", "JKS");
+		connector.setAttribute("keystoreFile", keystoreName);
+		connector.setAttribute("truststorePass", keystorePass);
+		connector.setAttribute("truststoreType", "JKS");
+		connector.setAttribute("truststoreFile", truststoreName);
+		connector.setAttribute("clientAuth", "true");
+		connector.setAttribute("sslProtocol", "TLS");
+		connector.setAttribute("SSLEnabled", "true");
+		connector.setAttribute("maxThreads", "200");
+
+		decorateConnector(connector);
+
+		return connector;
 	}
 }
