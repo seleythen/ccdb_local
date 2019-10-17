b8efcc6e70f985b7b912ac821437b7b319988c3e
Costin Grigoras <costin.grigoras@cern.ch>
2019-10-17 10:29:13 +0200

    Rely on the same notifiers to do the actual removal of files

--- src/ch/alice/o2/ccdb/servlets/AsyncPhysicalRemovalThread.java
+++ src/ch/alice/o2/ccdb/servlets/AsyncPhysicalRemovalThread.java
@@ -1,20 +1,7 @@
 package ch.alice.o2.ccdb.servlets;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
-
-import alien.catalogue.GUID;
-import alien.catalogue.LFN;
-import alien.catalogue.LFNUtils;
-import alien.catalogue.PFN;
-import alien.catalogue.access.AccessType;
-import alien.catalogue.access.AuthorizationFactory;
-import alien.io.protocols.Factory;
-import alien.io.protocols.Xrootd;
-import alien.se.SE;
-import alien.se.SEUtils;
 
 /**
  * Physical removal of files is expensive so don't make the client wait until it happens but instead return control immediately and do the physical removal asynchronously
@@ -35,7 +22,8 @@ public class AsyncPhysicalRemovalThread extends Thread {
 				object = asyncPhysicalRemovalQueue.take();
 
 				if (object != null)
-					deleteReplicas(object);
+					for (final SQLNotifier notifier : SQLBacked.getNotifiers())
+						notifier.deletedObject(object);
 			}
 			catch (final InterruptedException e) {
 				e.printStackTrace();
@@ -58,54 +46,5 @@ public class AsyncPhysicalRemovalThread extends Thread {
 
 	static void queueDeletion(final SQLObject object) {
 		getInstance().asyncPhysicalRemovalQueue.offer(object);
-	}
-
-	static void deleteReplicas(final SQLObject object) {
-		for (final Integer replica : object.replicas)
-			if (replica.intValue() == 0) {
-				// local file
-				final File f = object.getLocalFile(false);
-
-				if (f != null)
-					f.delete();
-			}
-			else
-				if (replica.intValue() < 0) {
-					final LFN l = LFNUtils.getLFN(object.getAddress(Integer.valueOf(-1), false).iterator().next());
-
-					if (l != null)
-						l.delete(true, false);
-				}
-				else {
-					final SE se = SEUtils.getSE(replica.intValue());
-
-					if (se != null) {
-						final GUID guid = object.toGUID();
-
-						if (guid == null)
-							continue;
-
-						for (final String address : object.getAddress(replica)) {
-							final PFN delpfn = new PFN(address, guid, se);
-
-							final String reason = AuthorizationFactory.fillAccess(AuthorizationFactory.getDefaultUser(), delpfn, AccessType.DELETE, true);
-
-							if (reason != null) {
-								System.err.println("Cannot get the access tokens to remove this pfn: " + delpfn.getPFN() + ", reason is: " + reason);
-								continue;
-							}
-
-							final Xrootd xrd = Factory.xrootd;
-
-							try {
-								if (!xrd.delete(delpfn))
-									System.err.println("Cannot physically remove this file: " + delpfn.getPFN());
-							}
-							catch (final IOException e) {
-								System.err.println("Exception removing this pfn: " + delpfn.getPFN() + " : " + e.getMessage());
-							}
-						}
-					}
-				}
 	}
 }
